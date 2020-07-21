7e29667f2cb67f2b5ae7b247b4b52a6d79fdab0f
arekczap <arekczapski@gmail.com>
2020-07-22 00:01:19 +0200

    task done finished, next todo filter tasks from not done to done in list

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const getDom = {
   input: document.querySelector(".input__task"),
   labelInput: document.querySelector(".input__label"),
   addButton: document.querySelector(".button__add-task"),
-  doneButton: document.querySelector(".finish__button"),
   taskList: document.querySelector(".tasks__list"),
 };
 
 const tasks = [];
+let finishedTask;
 
 // dodawanie label w przypadku nie wprowadzenia wartości
 const addInvalidLabel = (state) => {
@@ -26,49 +26,62 @@ const reloadArray = () => {
     task.dataset.id = index;
     getDom.taskList.append(task);
   });
+
+  getDom.input.value = "";
 };
 
 const createLiElement = () => {
   const liElement = document.createElement("li");
   liElement.className = "task__item";
   liElement.innerHTML = `<div class="task__container"><li class="task">${getDom.input.value}</li><div class="task__content"><button class="finish__button button"><span class="icon-ok"></span></button><button class="remove__button button "><span class="icon-trash-empty"></span></button></div></div>`;
-
   return liElement;
 };
+
+const removeTask = (e) => {
+  const buttonLi = e.target.parentNode.parentNode.parentNode.parentNode;
+  buttonLi.remove();
+  tasks.splice(buttonLi.dataset.id, 1);
+  reloadArray();
+};
+
+const finishTask = (e) => {
+  finishedTask = true;
+  const finishButton = e.target.parentNode;
+  const taskValue = e.target.parentNode.parentNode.parentNode.firstChild;
+  finishButton.classList.toggle("finished");
+
+  if (taskValue.style.textDecoration === "line-through") {
+    taskValue.style.textDecoration = "none";
+    taskValue.style.opacity = "1";
+  } else {
+    taskValue.style.textDecoration = "line-through";
+    taskValue.style.opacity = ".5";
+  }
+};
+
 const addTask = (e) => {
   e.preventDefault();
-
   //sprawdzanie czy input nie jest pusty czy nie ma białych spacji
   if (getDom.input.value.trim() == "" || getDom.input.value.trim() == null || getDom.input.value.trim() == " ") {
     addInvalidLabel("true");
     getDom.input.value = "";
   } else {
     addInvalidLabel("false");
-
     //tworzenie elementu li
     var liElement = createLiElement();
-
     //dodawanie taska do tablicy
     tasks.push(liElement);
-
     // reload tablicy, odświeżenie id
     reloadArray();
 
-    getDom.input.value = "";
+    //usuwanie tasku
     liElement.querySelector(".icon-trash-empty").addEventListener("click", removeTask);
+    //ukończenie tasku
+    liElement.querySelector(".icon-ok").addEventListener("click", finishTask);
   }
 };
 
-const removeTask = (e) => {
-  const buttonLi = e.target.parentNode.parentNode.parentNode.parentNode;
-
-  buttonLi.remove();
-  tasks.splice(buttonLi.dataset.id, 1);
-
-  reloadArray();
-};
-
-getDom.addButton.addEventListener("click", addTask);
-
 //event na nacisnięcie przycisku po czym label w formularzu zostanie schowana
 getDom.input.addEventListener("keypress", () => addInvalidLabel("false"));
+//dodanie tasku
+getDom.addButton.addEventListener("click", addTask);
