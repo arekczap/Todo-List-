7f40381837c0d2f13cea404216bd86166eef5975
arekczap <arekczapski@gmail.com>
2020-07-29 23:09:40 +0200

    added style changes, search bar - alghoritm in progress

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const getDom = {
   input: document.querySelector(".input__task"),
+  searchInput: document.querySelector('.search__input'),
   labelInput: document.querySelector(".input__label"),
   addButton: document.querySelector(".button__add-task"),
   taskList: document.querySelector(".tasks__list"),
@@ -12,7 +13,7 @@ let sortedArray = [];
 let finishedTask;
 
 // dodawanie label w przypadku nie wprowadzenia wartości
-const addInvalidLabel = (state) => {
+addInvalidLabel = (state) => {
 
 
   state == "true" ?
@@ -21,7 +22,7 @@ const addInvalidLabel = (state) => {
 
 };
 
-const reloadArray = () => {
+reloadArray = () => {
   getDom.taskList.textContent = "";
   newTasksWithFinished.length = 0;
   newTasksWithoutFinished.length = 0;
@@ -29,8 +30,6 @@ const reloadArray = () => {
 
   tasks.forEach((task, index) => {
     task.dataset.finished == "true" ? newTasksWithFinished.push(task) : newTasksWithoutFinished.push(task);
-
-
     task.dataset.id = index;
   });
 
@@ -43,21 +42,21 @@ const reloadArray = () => {
   getDom.input.value = "";
 };
 
-const createLiElement = () => {
+createLiElement = () => {
   const liElement = document.createElement("li");
   liElement.className = "task__item";
   liElement.innerHTML = `<div class="task__container"><li class="task">${getDom.input.value}</li><div class="task__content"><button class="finish__button button"><span class="icon-ok"></span></button><button class="remove__button button "><span class="icon-trash-empty"></span></button></div></div>`;
   return liElement;
 };
 
-const removeTask = (e) => {
+removeTask = (e) => {
   const buttonLi = e.target.parentNode.parentNode.parentNode.parentNode;
   buttonLi.remove();
   tasks.splice(buttonLi.dataset.id, 1);
   reloadArray();
 };
 
-const finishTask = (e) => {
+finishTask = (e) => {
   const finishButton = e.target.parentNode;
   const taskValue = e.target.parentNode.parentNode.parentNode.firstChild;
   const idTaskLi = e.target.parentNode.parentNode.parentNode.parentNode;
@@ -70,7 +69,24 @@ const finishTask = (e) => {
   reloadArray();
 };
 
-const addTask = (e) => {
+filterTasks = (e) => {
+  const searchText = e.target.value.toLowerCase();
+  let newArr = sortedArray;
+  newArr = newArr.filter(element => {
+    // element.textContent.toLowerCase().includes(searchText);
+    console.log(element.textContent)
+    element.textContent.toLowerCase() == searchText;
+  });
+
+  console.log("search: " + searchText)
+  console.log("new arr: " + sortedArray)
+
+
+  // newArr.length = 0;
+
+}
+
+addTask = (e) => {
   e.preventDefault();
   //sprawdzanie czy input nie jest pusty czy nie ma białych spacji
   if (getDom.input.value.trim() == "" || getDom.input.value.trim() == null || getDom.input.value.trim() == " ") {
@@ -98,5 +114,10 @@ const addTask = (e) => {
 
 //event na nacisnięcie przycisku po czym label w formularzu zostanie schowana
 getDom.input.addEventListener("keypress", () => addInvalidLabel("false"));
+
+
+//filtrowanie taskow
+getDom.searchInput.addEventListener('input', filterTasks);
+
 //dodanie tasku
 getDom.addButton.addEventListener("click", addTask);
